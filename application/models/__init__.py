e5c6dc0571d55f196d463fddf51db078485a4625
Juan Matthys Uys <opyate@gmail.com>
2015-07-10 12:37:33 +0100

    template for profile

--- application/models/__init__.py
+++ application/models/__init__.py
@@ -1,5 +1,5 @@
 from flask.ext.sandboy import Sandboy
-from flask import current_app
+from flask import current_app, request
 
 from .profile import Profile
 from .service import Service
@@ -8,19 +8,26 @@ from flask.ext.pushrod import pushrod_view
 
 
 class APIScaffold(object):
-
     def init_app(self, app, db, auth):
         def get_jinja_template():
-            current_app.logger.info('calling pushrod')
-            return 'base.html'
+            """
+            # /profile/42 == ['','profile','42']
+            # /profile == ['','profile']
+            """
+            parts = request.path.rsplit('/')
+            tpl = parts[1] + '.html'
+
+            current_app.logger.debug(
+                'pushrod retrieving template=%s for path=%s' %
+                (tpl, request.path))
+            return tpl
 
         decorators = [
             auth.login_required,
             pushrod_view(jinja_template=get_jinja_template)
         ]
 
-        sandboy = Sandboy(app, db,
-            [Profile, Service, Notification],
-            decorators=decorators,
-            renderer=None)
+        sandboy = Sandboy(app, db, [Profile, Service, Notification],
+                          decorators=decorators,
+                          renderer=None)
         return sandboy
